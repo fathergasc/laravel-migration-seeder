8f6b9dd070157288014eb7e05af7cd2858cde1bf
fathergasc <quaglioniandrea@gmail.com>
2022-09-29 14:41:37 +0200

    created migration with all keys requested

---
+++ database/migrations/2022_09_29_113630_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('operator', 60);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->unsignedTinyInteger('train_code');
+            $table->unsignedTinyInteger('cars');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
